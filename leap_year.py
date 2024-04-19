be090f2f5a08f7cf2507b11fcda20dd78e945c20
Juan Prieto <162722098+Juanp-r@users.noreply.github.com>
2024-04-19 10:11:41 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -2,10 +2,10 @@ def leap_year():
     print("TO DO")
     año=int(input("Ingrese un año:  "))
     if (año%100)==0 and(año%400)==0:
-            print(f"el año {año} es biciesto")
+            print(f"El año {año} es biciesto")
     elif (año%4)==0 and (año%100!=0):
-        print(f"el año {año} es biciesto")
+        print(f"El año {año} es biciesto")
     else :
-        print("el año no es biciesto")
+        print("El año no es biciesto")
 
 
